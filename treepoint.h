d998c3af51409574720b0e15991c1cc6e28cf739
Aleksandr Milakin <milakins@bk.ru>
2018-07-03 14:29:43 +0300

    Add files via upload

---
+++ treepoint.h
@@ -0,0 +1,30 @@
+#ifndef TREEPOINT_H
+#define TREEPOINT_H
+
+#include <QObject>
+#include <QGraphicsItem>
+#include <QPainter>
+#include <string>
+
+
+class treePoint : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+public:
+    explicit treePoint(int x, int y, std::string text, int freq, QObject *parent = 0);
+    ~treePoint();
+    int x;
+    int y;
+    int length;
+    int freq;
+signals:
+    void clicked();
+public slots:
+
+private:
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    std::string text;
+};
+
+#endif // TREEPOINT_H
